6d3ad4961b64e308dcf026a7241065bf6f9423f1
Evandro Costa <evandro.souzac@gmail.com>
2024-01-22 18:14:33 -0300

    Connection to local database

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+import { Client } from 'pg';
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
