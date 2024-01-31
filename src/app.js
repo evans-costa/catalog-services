a7e03e879f0dd84e4e56c3d0c8ea02cd31571d9c
Evandro Costa <evandro.souzac@gmail.com>
2024-01-30 23:02:27 -0300

    Use error handling and export routes on express app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import 'express-async-errors';
+import express from 'express';
+import productRoutes from './routes/product.routes';
+import categoryRoutes from './routes/category.routes';
+import { AppError } from './errors/AppError';
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const app = express();
+app.use(express.json());
+
+app.use('/products', productRoutes);
+app.use('/categories', categoryRoutes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  return res.status(500).json({
+    message: 'Internal server error',
+  });
 });
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
 });
+
+export default app;
