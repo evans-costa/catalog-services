555b54ed7208eb64111052cede92f57b2aff1f22
Evandro Costa <evandro.souzac@gmail.com>
2024-01-30 23:00:33 -0300

    Require `dotenv` on env variables on database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import { Client } from 'pg';
 
 async function query(queryObject) {
   const client = new Client({
-    user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
   });
 
   await client.connect();
